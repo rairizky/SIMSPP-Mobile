de0a07fb01a3e829b257049a2ad2627e462d80b0
rairizky <32klolita@gmail.com>
2020-05-09 11:53:50 +0700

    fix manifest

--- app/src/main/java/com/graphtech/simspp/network/ApiURL.kt
+++ app/src/main/java/com/graphtech/simspp/network/ApiURL.kt
@@ -2,7 +2,7 @@ package com.graphtech.simspp.network
 
 object ApiURL {
     private val BASE_API = "http://192.168.137.83:8000/api/"
-    private val BASE_API_TEST = "http://simspp.herokuapp.com/api/"
+    private val BASE_API_TEST = "https://simspp.herokuapp.com/api/"
 
     fun getLogin(email: String, password: String): String {
         return BASE_API_TEST+"signin?email=${email}&password=${password}"
